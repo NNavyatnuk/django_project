455a02aa878347491bc924cfe365787a42af8b1f
NNavyatnuk <nnavytanuk@gmail.com>
2025-10-03 22:38:32 +0200

    working code

--- my_project/home/views.py
+++ my_project/home/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Привіт! Це додаток Home.")
+def home_page(request):
+    return render(request, "home.html")
