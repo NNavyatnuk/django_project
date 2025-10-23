08bbea27dadbfe360d08e9844927d7f3c757b94f
GFSA66 <gf66sorokina@gmail.com>
2025-10-24 00:28:01 +0300

    appended urls

--- Huod_Market/urls.py
+++ Huod_Market/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    
 ]
+
+admin.site.site_header = "Administration Club"
+admin.site.site_title = "Admin Club Portal"
+admin.site.index_title = "Club Membership Area"
