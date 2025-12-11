aa5e0e37ebe183535b0cae8ca17c270bb83bf0bf
Mata0nui <volodimirgontar2008@example.com>
2025-12-11 22:35:02 +0200

    NEW STYLE

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('cart', cart_page, name='cart'),
     path('bestseller', bestseller_page, name='bestseller'),
     path('single', single_page, name='single'),
+    path('login_user', login_user, name='login'),
+    path('logout_user', logout_user, name='logout'),
+    path('register_user', register_user, name='register_user'),
     re_path(r'^.*$', error_404_page, name='404'),
 ]
